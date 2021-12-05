f6e2a12b065e917585ec1cecd7bfee45aa963471
SerenityJW <94766015+SerenityJW@users.noreply.github.com>
2021-12-05 15:19:20 -0800

    Add files via upload

---
+++ jingyuan/main.js
@@ -0,0 +1,14 @@
+/**
+ * Global Variables
+ */
+var _data = [];
+
+/**
+ * Do the following when the browser window loads
+ */
+window.onload = function(){
+    // loadData("betterlifeindex.csv");
+    // loadData("street-trees-newnew.csv");
+    loadData("whole_data.json");
+};
+
